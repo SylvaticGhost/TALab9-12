76385e355ecb55b55d2cc3f362cfd0c69a1c8b51
SylvaticGhost <143171907+SylvaticGhost@users.noreply.github.com>
2024-05-20 16:17:47 +0300

    add test lab 10 - 11

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,9 +2,14 @@
 import radixSort from "@/lib/sorts/sort-for-long-key/radixSort";
 import bucketSort from "@/lib/sorts/sort-for-long-key/bucketSort";
 import {testLinear} from "@/tests/linearSortTest";
+import mergeSort from "@/lib/sorts/sort-for-large/mergeSort";
+import smoothSort from "@/lib/sorts/sort-for-large/smoothSort";
+import quickSort from "@/lib/sorts/quickSort";
 
 export default function testPage() {
     console.log("Test Page");
+    
+    //testRadix can test all sorts
     testRadix(radixSort).then((result) => {
         console.log("Radix Sort Test");
         console.log(result);
@@ -15,6 +20,23 @@ export default function testPage() {
         console.log("Bucket Sort Test");
         console.log(result);
     });
+    
+    
+    testRadix(mergeSort).then((result) => {
+        console.log("Merge Sort Test");
+        console.log(result);
+    });
+    
+    testRadix(smoothSort).then((result) => {
+        console.log("Smooth Sort Test");
+        console.log(result);
+    });
+    
+    testRadix(quickSort).then((result) => { 
+        console.log("Quick Sort Test");
+        console.log(result);
+    });
+    
     
     return (
         <div>
